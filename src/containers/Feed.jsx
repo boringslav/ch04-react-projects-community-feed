de9365a91a18be6affbfc7c6cabfef71ceaa5fac
Borislav Stoyanov <bostoy@protonmail.com>
2021-06-13 14:02:37 +0300

    change pages

--- src/containers/Feed.jsx
+++ src/containers/Feed.jsx
@@ -33,43 +33,51 @@ border-radius:5px;
 `
 
 function Feed(props) {
+  const { match } = props
   const query = queryString.parse(props.location.search)
+
   const [data, setData] = useState([])
-  const [page, setPage] = useState()
+  const [page, setPage] = useState(1)
+  const [prevPage, setPrevPage] = useState(1)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState('')
 
   const ROOT_API = 'https://api.stackexchange.com/2.2/';
 
-  useEffect(() => {
-    (async () => {
-
-      try {
-        const data = await fetch(
-          `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow${(page) ? `&page=${page}` : ''}`,
-        )
-        const dataJSON = await data.json()
 
 
-        if (dataJSON) {
-          setData(dataJSON)
-          setLoading(false)
-        }
-      } catch (error) {
-        setLoading(true)
-        setError(error.message)
+  async function fetchAPI(page) {
+    try {
+      const data = await fetch(
+        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow${(page) ? `&page=${page}` : ''}`,
+      )
+      const dataJSON = await data.json()
+
+
+      if (dataJSON) {
+        setData(dataJSON)
+        setLoading(false)
       }
+    } catch (error) {
+      setLoading(true)
+      setError(error.message)
+    }
+  }
 
-    })()
+  useEffect(() => {
+    fetchAPI(page)
   }, [])
 
   useEffect(() => {
-    if (query.page) {
+    if (query.page != page) {
+      setPrevPage(page)
       setPage(parseInt(query.page))
+      fetchAPI(page)
+
     } else {
       setPage(1)
     }
-  }, [])
+  }, [query.page])
 
 
   if (loading || error) {
@@ -85,8 +93,8 @@ function Feed(props) {
         </CardLink>
       ))}
       <PaginationBar>
-        <PaginationLink>Previous</PaginationLink>
-        <PaginationLink>Next</PaginationLink>
+        {page > 1 && <PaginationLink to={`${match.url}?page=${page - 1}`}>Previous</PaginationLink>}
+        {data.has_more && <PaginationLink to={`${match.url}?page=${page + 1}`}>Next</PaginationLink>}
       </PaginationBar>
     </FeedWrapper>
   )
