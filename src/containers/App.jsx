2cb22a6817895f1012882d72d46099ca70121d32
Borislav Stoyanov <bostoy@protonmail.com>
2021-06-13 16:09:21 +0300

    config + fix server + change browserRouter component location
    When the application is running client-side (using npm start), it will still need
    to use BrowserRouter, so therefore the wrapping of the Route components
    should be moved from App to the src/index.js file

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 import Header from '../components/Header/Header';
 import Feed from './Feed';
@@ -27,13 +27,11 @@ function App() {
       <GlobalStyle />
       <AppWrapper>
         <Header />
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Feed} />
-            <Route path='/questions/:id' component={Question} /> //questions:id before question because switch matches first url path
-            <Route path="/questions" component={Feed} />
-          </Switch>
-        </Router>
+        <Switch>
+          <Route exact path='/' component={Feed} />
+          <Route path='/questions/:id' component={Question} /> //questions:id before question because switch matches first url path
+          <Route path="/questions" component={Feed} />
+        </Switch>
       </AppWrapper>
     </>
   )
